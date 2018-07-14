2d5f1cdeb5e6c87b6b89a6904280689e46dbc7a9
HelgyIngvarsson <olehgavrilin@gmail.com>
2018-07-15 00:51:51 +0300

    Added lists.

--- src/components/leftPanel/LeftPanelComponent.js
+++ src/components/leftPanel/LeftPanelComponent.js
@@ -1,5 +1,9 @@
 import React,{Component} from 'react'
 import './leftPanel.scss'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 
 export default class LeftPanelComponent extends Component{
     constructor(props){
@@ -9,7 +13,23 @@ export default class LeftPanelComponent extends Component{
     render(){
         return(
             <div className='left-bar'>
-
+                <List component="nav">
+                    <ListItem button>
+                        <ListItemText primary="Inbox" />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemText primary="Drafts" />
+                    </ListItem>
+                </List>
+                <Divider />
+                <List component="nav">
+                    <ListItem button>
+                        <ListItemText primary="Trash" />
+                    </ListItem>
+                    <ListItem button component="a" href="#simple-list">
+                        <ListItemText primary="Spam" />
+                    </ListItem>
+                </List>
             </div>
         );
     }
